29685a1496568bc05f862dd8048aa3e8e8fcbe90
minheekangg <minheekangg@gmail.com>
2018-11-17 22:16:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+  if ((distance - 42) <0) {
+    return ((distance - 42) * -1);
+      }
+    else {
+      return (distance - 42);
+      }
+};
+
+
+function distanceFromHqInFeet (someValue) {
+    return (distanceFromHqInBlocks(someValue) * 264);
+  }
+
+function distanceTravelledInFeet(uptown, downtown){
+  if (uptown - downtown < 0) {
+      return ((uptown - downtown) * -1) * 264;
+  }
+  else {
+      return ((uptown - downtown) * 264);
+  }
+}
+
+
+function calculatesFarePrice(start, destination) {
+  if ((distanceTravelledInFeet(start, destination)) < 400){
+    return 0;
+  } else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000) {
+    return ((distanceTravelledInFeet(start, destination) - 400) * .02);
+  } else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+    return 25;
+  } else
+    return 'cannot travel that far';
+
+}
