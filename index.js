17b1f35bbb008f58a9d864421b3e673565adc5be
sarah pai <pai.sarah89@gmail.com>
2018-11-18 03:19:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const headquarters = 42;
+
+function distanceFromHqInBlocks(distance) {
+ return (distance > headquarters) ? distance - headquarters : headquarters - distance;
+}
+
+function distanceFromHqInFeet(feet) {
+  return distanceFromHqInBlocks(feet) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  return (start > end) ? (start - end)*264 : (end - start)*264;
+}
+
+function calculatesFarePrice(start, destination) {
+
+  let travelDistance = distanceTravelledInFeet(start, destination);
+    if (travelDistance < 400) {
+      return 0;
+    }
+    else if (travelDistance > 400 && travelDistance < 2000) {
+      return (travelDistance - 400) * 0.02;
+    }
+    else if (travelDistance > 2000 && travelDistance < 2500) {
+      return 25;
+  } else {
+      return 'cannot travel that far';
+  }
+}
